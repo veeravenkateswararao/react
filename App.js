38819d66d88e8b22aab1da4a4403fe5778084515
Super User <root@ip-172-31-44-96.ap-south-1.compute.internal>
2026-03-04 09:20:35 +0000

    vve

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h2>Welcomeo</h2>
+      <p>This is a sample React application.</p>
+    </div>
+  );
+}
+
+export default App;
